ff1d82c5b0923236fc885a18ef503515cb7161ba
SebastianClayton <115326240+SebastianClayton@users.noreply.github.com>
2023-01-13 20:55:49 -0500

    Edits terry teleop to have a working arm. Controls are on controller 2, right stick to control it. Right bumper tries to hold it up, but needs testing on if it's too fast. A and B should open and close claw, but it sure doesn't work! It's on the todo.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TerryTeleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TerryTeleop.java
@@ -2,8 +2,10 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
 
-@TeleOp(name = "better \uD83E\uDD5A")
+@TeleOp(name = "\uD83C\uDF56") // ham emoji
 public class TerryTeleop extends LinearOpMode {
     private TerryHardware terryHardware;
     private double defaultSensitivity = 1.0;
@@ -11,8 +13,11 @@ public class TerryTeleop extends LinearOpMode {
     private double turboSensitivity = 1.5;
     private double currentSensitivity = 1.0;
 
+    private DcMotor arm;
+
     @Override
     public void runOpMode() {
+        arm = hardwareMap.get(DcMotor.class, "arm");
         terryHardware = new TerryHardware(hardwareMap);
         terryHardware.initHardware();
         waitForStart();
@@ -21,15 +26,23 @@ public class TerryTeleop extends LinearOpMode {
             //ftc judges please understand this is a inside joke
             terryHardware.driveRobot(-gamepad1.left_stick_y * currentSensitivity, gamepad1.left_stick_x * currentSensitivity, gamepad1.right_stick_x * currentSensitivity);
 
+            arm.setPower(-gamepad2.right_stick_y * 0.25);
+
+            while(gamepad2.right_bumper) {
+                arm.setPower(-0.1); //TODO: needs to be slower. Fixed direction, do not change this to positive lol
+            }
+
             //claw controls
-            while (gamepad1.a) {
+            //TODO: this does not work at all for some reason. either claw is not defined, or you need a new servo.
+            //probably servo knowing our luck honestly
+            while (gamepad2.a) {
                 //yo bass pls i need claw stuff im gonna cry :-;
                 //Make this one the open claw, DIS OPEN CLAW UWU
                 terryHardware.setClawPosition(0.5);
             }
             //jesse, jesse we need a setClawPosition. JESSE!
             //Make this on close the claw, just like me closed off to commitment
-            while (gamepad1.b) {
+            while (gamepad2.b) {
                 terryHardware.setClawPosition(0);
             }
             if (gamepad1.right_bumper) {
