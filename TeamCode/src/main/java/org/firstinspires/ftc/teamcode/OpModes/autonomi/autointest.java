28ceb1fb6b8d091dcf56cfa2b5c8fc4843738734
Garnet Squadron <garnet@garnet-11444>
2024-10-18 19:14:59 -0400

    auto intake class test

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/autonomi/autointest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/autonomi/autointest.java
@@ -1,34 +1,20 @@
 package org.firstinspires.ftc.teamcode.OpModes.autonomi;
-import androidx.annotation.NonNull;
-import com.acmerobotics.dashboard.config.Config;
-import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
-import com.acmerobotics.roadrunner.Action;
-import com.acmerobotics.roadrunner.ftc.LynxFirmware;
-import com.arcrobotics.ftclib.controller.PIDController;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorEx;
-import com.qualcomm.robotcore.hardware.HardwareMap;
-import com.qualcomm.robotcore.hardware.Servo;
-import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
 import org.firstinspires.ftc.robotcore.external.stream.CameraStreamSource;
 import org.firstinspires.ftc.teamcode.SampleDetectionPipelinePNP;
-import org.opencv.core.Core;
-import org.opencv.core.Mat;
-import org.opencv.imgproc.Imgproc;
 import org.openftc.easyopencv.OpenCvCamera;
 import org.openftc.easyopencv.OpenCvCameraFactory;
 import org.openftc.easyopencv.OpenCvCameraRotation;
-import org.openftc.easyopencv.OpenCvPipeline;
 import org.openftc.easyopencv.OpenCvWebcam;
-import java.util.ArrayList;
+
 @TeleOp(name="auto intake test", group = "test")
 
 //imports from vision.java
 
-    public class autointaketest extends LinearOpMode {
+    public class autointest extends LinearOpMode {
         OpenCvWebcam webcam1 = null;
         //public OpenCvCamera cam1;
         public CameraStreamSource camera;
