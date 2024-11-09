b36825e6d38f8e43bad4fcd7a797e11fe6376322
Nathan <nathan.keisler23@gmail.com>
2024-11-08 20:23:16 -0500

    moved autointest.java into Tests folder

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Tests/autointest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Tests/autointest.java
@@ -83,7 +83,7 @@ public class autointest extends LinearOpMode {
 
                 else if(result == EJECTING) {
 
-                    long duration = 500;
+                    long duration = 1000;
                     long startTime = System.currentTimeMillis();
 
                     while (System.currentTimeMillis() - startTime < duration) {
