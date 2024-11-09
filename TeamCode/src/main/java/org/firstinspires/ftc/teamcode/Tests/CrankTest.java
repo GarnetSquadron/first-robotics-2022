9146ff37dd7609b46468e74ecef9315067d84ab2
Garnet Squadron <garnetsquadron@gmail.com>
2024-11-09 10:48:39 -0500

    CrankSlide Made

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Tests/CrankTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Tests/CrankTest.java
@@ -1,10 +1,12 @@
 package org.firstinspires.ftc.teamcode.Tests;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import org.firstinspires.ftc.teamcode.Subsystems.CrankSlideSubSystem;
 
 
+@TeleOp(name = "crankTest", group = "tests")
 public class CrankTest extends OpMode {
     CrankSlideSubSystem crank;
     @Override
