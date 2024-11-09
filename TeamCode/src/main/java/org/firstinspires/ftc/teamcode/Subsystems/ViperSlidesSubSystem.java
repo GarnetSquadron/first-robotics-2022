027cd4a005ca3b551280c5316b8cca52aa3a343f
Garnet Squadron <garnetsquadron@gmail.com>
2024-11-09 13:08:48 -0500

    VIPERS

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/ViperSlidesSubSystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/ViperSlidesSubSystem.java
@@ -1,28 +1,53 @@
 package org.firstinspires.ftc.teamcode.Subsystems;
 
+import com.arcrobotics.ftclib.hardware.motors.Motor;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.arcrobotics.ftclib.command.SubsystemBase;
 
 public class ViperSlidesSubSystem extends SubsystemBase{
-    private final DcMotor l;
-    private final DcMotor r;
-    private final double MaxPos = 200;
-    private final double MinPos = 0;
-    public ViperSlidesSubSystem(HardwareMap hardwareMap, String name1, String name2, String name3){
-        l = hardwareMap.get(DcMotor.class,name1);
-        r = hardwareMap.get(DcMotor.class,name2);
+    private final Motor l;
+    //private final Motor r;
+    private final int MaxPos = -4000;
+    private final int MinPos = 0;
+    private double posCoefficient = 0.05;
+    public ViperSlidesSubSystem(HardwareMap hardwareMap, String name1, String name2){
+        l = hardwareMap.get(Motor.class,name1);
+        //r = hardwareMap.get(Motor.class,name2);
 
     }
-    public void MoveToPosition(){
+    public void runMotorToPosition(int pos, double PC, Motor motor){
+        // set the run mode
+        motor.setRunMode(Motor.RunMode.PositionControl);
 
+// set and get the position coefficient
+        motor.setPositionCoefficient(PC);
+        double kP = motor.getPositionCoefficient();
+
+// set the target position
+        motor.setTargetPosition(pos);      // an integer representing
+        // desired tick count
+
+        motor.set(0);
+
+// set the tolerance
+        motor.setPositionTolerance(13.6);   // allowed maximum error
+
+// perform the control loop
+        while (!motor.atTargetPosition()) {
+            motor.set(0.75);
+        }
+        motor.stopMotor(); // stop the motor
+    }
+
+    public void runToPos(int pos){
+        runMotorToPosition(pos,posCoefficient,l);
     }
     public void Extend() {
-        l.setPower(-0.25);
-        r.setPower(+0.25);
+        runToPos(MaxPos);
     }
     public void Return() {
-        l.setPower(+0.25);
-        r.setPower(-0.25);
+       runToPos(MinPos);
     }
 }
