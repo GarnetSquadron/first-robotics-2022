8d55c2c27eddce64101005332c39f75f2208ce6f
Garnet Squadron <garnetsquadron@gmail.com>
2024-11-09 11:04:13 -0500

    CrankSlide Made

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/CrankSlideSubSystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/CrankSlideSubSystem.java
@@ -7,15 +7,22 @@ import com.qualcomm.robotcore.hardware.HardwareMap;
 public class CrankSlideSubSystem extends SubsystemBase {
     private final Servo CrankL;
     private final Servo CrankR;
+    private double LeftMax = 0.3333;
+    private double RightMax = 1;
+    private double LeftMin = 1;
+    private double RightMin = 0.3333;
 
     public CrankSlideSubSystem(HardwareMap hardwareMap, String name1, String name2) {
         CrankL = hardwareMap.get(Servo.class, name1);
         CrankR = hardwareMap.get(Servo.class, name2);
-        CrankR.setDirection(Servo.Direction.REVERSE);
+
+    }
+    double getPos(double min, double max, double pos){
+        return min+pos*(max-min);
     }
     public void goToPos(double pos){
-        CrankL.setPosition(2*pos/3.0);
-        CrankR.setPosition(2*pos/3.0);
+        CrankL.setPosition(getPos(LeftMin,LeftMax,pos));
+        CrankR.setPosition(getPos(RightMin,RightMax,pos));
     }
     public void Extend() {
         goToPos(0);
