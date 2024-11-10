b750539757327d7b4792fab421f83ab888e2fbd3
Garnet Squadron <garnetsquadron@gmail.com>
2024-11-10 09:13:15 -0500

    Changed crank code to account for the odd number of teeth

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/CrankSlideSubSystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/CrankSlideSubSystem.java
@@ -7,10 +7,14 @@ import com.qualcomm.robotcore.hardware.HardwareMap;
 public class CrankSlideSubSystem extends SubsystemBase {
     private final Servo CrankL;
     private final Servo CrankR;
+    private int SplineTeeth = 25;
+    private double getToothSize(int teeth){
+        return 4.0/(teeth*3.0);
+    }
     private double LeftMax = 0.3333;
-    private double RightMax = 1;
+    private double RightMax = 1-getToothSize(SplineTeeth);
     private double LeftMin = 1;
-    private double RightMin = 0.3333;
+    private double RightMin = 0.3333-getToothSize(SplineTeeth);
 
     public CrankSlideSubSystem(HardwareMap hardwareMap, String name1, String name2) {
         CrankL = hardwareMap.get(Servo.class, name1);
