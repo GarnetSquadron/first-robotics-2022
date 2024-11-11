8d9acdbe1fac652af79cb00ebfbd297d9d1f45a2
Nathan14635 <nathan.keisler23@gmail.com>
2024-11-11 17:17:16 -0500

    Amended previous pushed code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/ServoAlignmentSub.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/ServoAlignmentSub.java
@@ -2,7 +2,7 @@ package org.firstinspires.ftc.teamcode.Subsystems;
 import com.arcrobotics.ftclib.command.SubsystemBase;
 import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.HardwareMap;
-//import com.qualcomm.robotcore.hardware.Servo;
+
 
 public class ServoAlignmentSub extends SubsystemBase {
     public final CRServo left;
