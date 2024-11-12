a7fb277f9f0e01f43be5a89d351ff32e6b8da59a
Garnet Squadron <garnetsquadron@gmail.com>
2024-11-12 18:18:16 -0500

    enums

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/commands/TriangleIntakeCommand.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/commands/TriangleIntakeCommand.java
@@ -6,11 +6,14 @@ import com.qualcomm.robotcore.robocol.Command;
 
 import org.firstinspires.ftc.teamcode.NonDriveHardware;
 import org.firstinspires.ftc.teamcode.Subsystems.TriangleIntake;
+import org.firstinspires.ftc.teamcode.enums.AlianceColor;
 
 public class TriangleIntakeCommand extends CommandBase {
     TriangleIntake triangleIntake;
-    TriangleIntakeCommand(TriangleIntake t){
+    AlianceColor alianceColor;
+    TriangleIntakeCommand(TriangleIntake t, AlianceColor a){
         triangleIntake = t;
+        alianceColor = a;
     }
     @Override
     public void execute() {
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/enums/AlianceColor.java
@@ -0,0 +1,6 @@
+package org.firstinspires.ftc.teamcode.enums;
+
+public enum AlianceColor {
+    RED,
+    BLUE
+}
