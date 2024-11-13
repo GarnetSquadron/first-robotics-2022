bcc14080372c2ff1faab99dbf84795eff11cf57e
Garnet Squadron <garnetsquadron@gmail.com>
2024-11-12 19:38:38 -0500

    ServoUpdate

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/ServoAlignmentSub.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/ServoAlignmentSub.java
@@ -2,24 +2,25 @@ package org.firstinspires.ftc.teamcode.Subsystems;
 import com.arcrobotics.ftclib.command.SubsystemBase;
 import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
 
 
 public class ServoAlignmentSub extends SubsystemBase {
-    public final CRServo left;
-    public final CRServo right;
+    public final Servo left;
+    public final Servo right;
     public ServoAlignmentSub(HardwareMap hardwareMap, String name1, String name2) {
-         left = hardwareMap.get(CRServo.class, name1);
-         right = hardwareMap.get(CRServo.class, name2);
+         left = hardwareMap.get(Servo.class, name1);
+         right = hardwareMap.get(Servo.class, name2);
     }
 
     public void Up() {
-        left.setPower(1);
-        right.setPower(1);
+        left.setPosition(1);
+        right.setPosition(0);
     }
 
     public void Down() {
-        left.setPower(-1);
-        right.setPower(-1);
+        left.setPosition(0);
+        right.setPosition(1);
     }
 }
 //commit
