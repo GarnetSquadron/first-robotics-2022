301db4c829a33e82912b4f8a34f352958a52cbe9
Garnet Squadron <garnetsquadron@gmail.com>
2024-11-12 19:43:22 -0500

    ServoUpdate

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Tests/JamesServoAlignment.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Tests/JamesServoAlignment.java
@@ -1,5 +1,6 @@
 package org.firstinspires.ftc.teamcode.Tests;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 
@@ -8,18 +9,20 @@ import org.firstinspires.ftc.teamcode.Subsystems.ServoAlignmentSub;
 
 @TeleOp(name="James Servo Alignment", group = "test")
 
-public class JamesServoAlignment extends LinearOpMode{
+public class JamesServoAlignment extends OpMode {
      ServoAlignmentSub ServoAlignment;
 
-    public void Onstart(){
-        ServoAlignment.Up();
-        sleep(5000);
-        ServoAlignment.Down();
+     @Override
+    public void loop() {
+        if (gamepad1.y) {
+            ServoAlignment.Up();
+        }
+        if (gamepad1.x) {
+            ServoAlignment.Down();
+        }
     }
-    public void runOpMode(){
+    public void init(){
         ServoAlignment = new ServoAlignmentSub(hardwareMap,"AlignServo1", "AlignServo2");
-        waitForStart();
-        Onstart();
     }
 }
 //commit
