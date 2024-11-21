6f5d702eb45b6f5d3e29f26594d5432ba7b77344
Garnet Squadron <garnetsquadron@gmail.com>
2024-11-21 16:42:28 -0500

    MeepMeep

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -1,11 +1,29 @@
 package com.example.meepmeeptesting;
 
+import com.acmerobotics.roadrunner.Action;
 import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.Rotation2d;
+import com.acmerobotics.roadrunner.Vector2d;
 import com.noahbres.meepmeep.MeepMeep;
 import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
 import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
 
 public class MeepMeepTesting {
+    static double getTangentAngle(Pose2d tgtPose, Pose2d beginPose){
+        return Math.PI+Math.atan((tgtPose.position.y-beginPose.position.y)/(tgtPose.position.x-beginPose.position.x));
+    }
+    static Action RotateMove(Pose2d beginPose, Vector2d tgtp, double spinRate,RoadRunnerBotEntity myBot){
+        Vector2d v=tgtp.div(2);
+        return myBot.getDrive().actionBuilder(beginPose)
+                .splineToSplineHeading(new Pose2d(beginPose.position.plus(tgtp.minus(beginPose.position).div(2)), Math.PI/2), Math.PI)
+                .splineToSplineHeading(new Pose2d(tgtp, 0), Math.PI)
+//                .strafeToSplineHeading(tgtp.times(1/4),2)
+//
+//                .strafeToSplineHeading(tgtp.times(2/4),4)
+//                .strafeToSplineHeading(tgtp.times(3/4),6)
+//                .strafeToSplineHeading(tgtp,8)
+                .build();
+    }
     public static void main(String[] args) {
         MeepMeep meepMeep = new MeepMeep(800);
 
@@ -15,10 +33,15 @@ public class MeepMeepTesting {
                 .build();
 
         Pose2d beginPose = new Pose2d(60,0,Math.PI);
-        Pose2d tgtPose = new Pose2d(20,20,Math.PI);
-        myBot.runAction(myBot.getDrive().actionBuilder(beginPose)
-                .splineToSplineHeading(tgtPose,-Math.atan((tgtPose.position.y-beginPose.position.y)/(tgtPose.position.x-beginPose.position.x)))
-                .build());
+        Pose2d tgtPose = new Pose2d(0,0,Math.PI/3);
+
+        myBot.runAction(
+                    RotateMove(beginPose,tgtPose.position,2,myBot)
+//                myBot.getDrive().actionBuilder(beginPose)
+//                    //.splineToSplineHeading(tgtPose,getTangentAngle(tgtPose,beginPose))
+//                    .splineToSplineHeading(new Pose2d(0,0,beginPose.heading.toDouble()+Math.PI/2),)
+//                    .build()
+                    );
 
         meepMeep.setBackground(MeepMeep.Background.FIELD_INTO_THE_DEEP_JUICE_DARK)
                 .setDarkMode(true)
