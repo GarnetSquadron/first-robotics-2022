b1b33b7083c5025bb42c88786fe7b0e5bd3e39d9
Nathan <nathan.keisler23@gmail.com>
2024-11-23 10:19:19 -0500

    Made an auto route -N

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/autonomi/NathanAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/autonomi/NathanAuto.java
@@ -3,25 +3,33 @@ package org.firstinspires.ftc.teamcode.OpModes.autonomi;
 import com.acmerobotics.roadrunner.Vector2d;
 
 public class NathanAuto {
+
+//               RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
+//                // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
+//                .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
+//                .build();
+//
+//        Pose2d beginPose = new Pose2d(-26,-70,Math.toRadians(180));
+//        Pose2d tgtPose = new Pose2d(20,20,Math.PI);
+//        myBot.runAction(myBot.getDrive().actionBuilder(beginPose)
 //                .splineToConstantHeading(new Vector2d(-34,-26),90)
-//            .turn(Math.toRadians(90))
-//            .waitSeconds(0.7)
+//                .waitSeconds(0.7)
 //                .splineToConstantHeading(new Vector2d(-55,-55),0)
-//            .turn(Math.toRadians(225))
-//            .waitSeconds(0.7)
+//                .turn(Math.toRadians(225))
+//                .waitSeconds(0.7)
 //                .splineToConstantHeading(new Vector2d(-44,-26),90)
-//            .turn(Math.toRadians(135))
-//            .waitSeconds(0.7)
+//                .turn(Math.toRadians(135))
+//                .waitSeconds(0.7)
 //                .splineToConstantHeading(new Vector2d(-55,-55),0)
-//            .turn(Math.toRadians(225))
-//            .waitSeconds(0.7)
+//                .turn(Math.toRadians(225))
+//                .waitSeconds(0.7)
 //                .splineToConstantHeading(new Vector2d(-54,-26),90)
-//            .turn(Math.toRadians(135))
-//            .waitSeconds(0.7)
+//                .turn(Math.toRadians(135))
+//                .waitSeconds(0.7)
 //                .splineToConstantHeading(new Vector2d(-55,-55),0)
-//            .turn(Math.toRadians(225))
-//            .waitSeconds(0.7)
+//                .turn(Math.toRadians(225))
+//                .waitSeconds(0.7)
 //                .splineToConstantHeading(new Vector2d(58,-62),0)
-//            .turn(Math.toRadians(45))
-//            .build());
+//                .turn(Math.toRadians(45))
+//                .build());
 }
