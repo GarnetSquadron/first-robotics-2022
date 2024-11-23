a7cffa41cf87244b2d87859e235533bff0eb653d
user <123@gmain>
2024-11-22 20:32:55 -0500

    Anthony commit auto

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/autonomi/AnthonyAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/autonomi/AnthonyAuto.java
@@ -1,17 +1,31 @@
 package org.firstinspires.ftc.teamcode.OpModes.autonomi;
+import com.acmerobotics.roadrunner.Action;
+import com.acmerobotics.roadrunner.ParallelAction;
 import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.SequentialAction;
+import com.acmerobotics.roadrunner.Trajectory;
+import com.acmerobotics.roadrunner.Vector2d;
+import com.acmerobotics.roadrunner.ftc.Actions;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-
-
+import com.acmerobotics.dashboard.config.Config;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import org.firstinspires.ftc.teamcode.MecanumDrive;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.ColorSensor;
 import com.qualcomm.robotcore.hardware.DcMotor;
-
-import org.firstinspires.ftc.teamcode.depricated.TriangleIntake.TriangleIntake;
-import org.firstinspires.ftc.teamcode.Subsystems.outake.ViperSlidesSubSystem;
+import com.qualcomm.robotcore.hardware.Servo;
+import org.firstinspires.ftc.teamcode.Subsystems.ColorSensorSubSystem;
+import org.firstinspires.ftc.teamcode.Subsystems.TriangleIntake;
+import org.firstinspires.ftc.teamcode.commands.TriangleIntakeCommand;
+import org.firstinspires.ftc.teamcode.Subsystems.ViperSlidesSubSystem;
+import org.firstinspires.ftc.teamcode.Subsystems.TriangleIntake;
+import org.firstinspires.ftc.teamcode.oldStuff.VoidsAndThings;
 
 public class AnthonyAuto extends LinearOpMode {
     TriangleIntake triangleIntake;
     ViperSlidesSubSystem viperSlidesSubsystem;
-    //TriangleIntake triangleIntake = new TriangleIntake(hardwareMap,"IntakeServo1", "IntakeServo2", "IntakeServo3","pivot");
+    TriangleIntake triangleIntake = new TriangleIntake(hardwareMap,"IntakeServo1", "IntakeServo2", "IntakeServo3","pivot");
     DcMotor lf;
     DcMotor rf;
     DcMotor lb;
@@ -21,25 +35,20 @@ public class AnthonyAuto extends LinearOpMode {
         //Raise Viperslides and score sample in highest bucket
         //Lower Viperslides
         //spline to sample
-//        Action path1 = drive.actionBuilder(beginPose)
-//                .setTangent(0)
-//                .splineToConstantHeading(new Vector2d(36,54),0)
-//                .build();
-//        Action path2 = drive.actionBuilder(new Pose2d(40,20,0))
-//                .setTangent(0)
-//                .splineToConstantHeading(new Pose2d(54,-36, 0)
-//                        .build();
+        MecanumDrive Drive = new MecanumDrive(hardwareMap,StartPose);
+        Action action1 = Drive.actionBuilder(StartPose)
+                .setTangent(0)
+                .splineToConstantHeading(new Vector2d(36,54),0)
+                .build();
+        Action action2 = Drive.actionBuilder(new Pose2d(36,54,0))
+                .setTangent(0)
+                .splineToConstantHeading(new Vector2d(54,-36),0)
+                .build();
 
-//        Action PATH =new ParallelAction(
-//                new SequentialAction(
-//                        new ParallelAction(
-//                                path1
-//                        ),
-//                        new ParallelAction(
-//                                path2
-//                        ),
-//                        )
-//        );
+
+        Actions.runBlocking(action1);
+        Actions.runBlocking(action2);
+
         //pick up sample
         //transfer sample to bucket
         //spline back to bucket
@@ -55,6 +64,6 @@ public class AnthonyAuto extends LinearOpMode {
 }
 
 
-
+    }
 
 //commit 12
