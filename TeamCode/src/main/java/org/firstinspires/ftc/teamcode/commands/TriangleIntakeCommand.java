7b36766c6be71649e8a5f9e6d7c99493d86d9781
Nathan <nathan.keisler23@gmail.com>
2024-11-23 08:29:45 -0500

    Changed code in TriangleIntakeCommand to use the crank slides, awaiting importation of pivot code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/commands/TriangleIntakeCommand.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/commands/TriangleIntakeCommand.java
@@ -2,7 +2,7 @@ package org.firstinspires.ftc.teamcode.commands;
 
 import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.telemetry;
 
-
+import org.firstinspires.ftc.teamcode.Subsystems.CrankSlideSubSystem;
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 import org.firstinspires.ftc.teamcode.Subsystems.ColorSensorSubSystem;
 import org.firstinspires.ftc.teamcode.Subsystems.TriangleIntake;
@@ -14,11 +14,13 @@ import org.firstinspires.ftc.teamcode.enums.Color;
 
 public class TriangleIntakeCommand extends CommandBase {
     TriangleIntake triangleIntake;
+    CrankSlideSubSystem crankSlideSubSystem;
     public Color alianceColor;
     ColorSensorSubSystem colorSensor;
     Telemetry TELEMETRY;
     public Color c;
     public TriangleIntakeCommand(TriangleIntake t, ColorSensorSubSystem c, Color a, Telemetry tel){
+
         triangleIntake = t;
         alianceColor = a;
         colorSensor = c;
@@ -48,10 +50,15 @@ public class TriangleIntakeCommand extends CommandBase {
 
         } else if (c == Color.YELLOW){
             triangleIntake.hold();
-            finished = true;
+            //run pivot for main arm
+            crankSlideSubSystem.Return();
+            triangleIntake.eject();
+            crankSlideSubSystem.Extend();
+            //run pivot for main arm
+
         } else if (c == alianceColor){
             triangleIntake.hold();
-            finished = true;
+
         }else{
             //telemetry.addLine("Else has been reached");
         }
