ef96af26a1029d3802ae3dca5ffbc1072cf57208
Nathan14635 <nathan.keisler23@gmail.com>
2024-11-24 15:04:10 -0500

    messed with ScrimmageAutoRoute and commented some unused hardware maps

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/autonomi/NathanAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/autonomi/NathanAuto.java
@@ -4,7 +4,7 @@ import com.acmerobotics.roadrunner.Vector2d;
 
 public class NathanAuto {
 
-//               RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
+//            RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
 //                // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
 //                .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
 //                .build();
@@ -14,19 +14,19 @@ public class NathanAuto {
 //        myBot.runAction(myBot.getDrive().actionBuilder(beginPose)
 //                .splineToConstantHeading(new Vector2d(-34,-26),90)
 //                .waitSeconds(0.7)
-//                .splineToConstantHeading(new Vector2d(-55,-55),0)
+//                .splineToConstantHeading(new Vector2d(-55,-55),10)
 //                .turn(Math.toRadians(225))
 //                .waitSeconds(0.7)
 //                .splineToConstantHeading(new Vector2d(-44,-26),90)
 //                .turn(Math.toRadians(135))
 //                .waitSeconds(0.7)
-//                .splineToConstantHeading(new Vector2d(-55,-55),0)
+//                .splineToConstantHeading(new Vector2d(-55,-55),10)
 //                .turn(Math.toRadians(225))
 //                .waitSeconds(0.7)
 //                .splineToConstantHeading(new Vector2d(-54,-26),90)
 //                .turn(Math.toRadians(135))
 //                .waitSeconds(0.7)
-//                .splineToConstantHeading(new Vector2d(-55,-55),0)
+//                .splineToConstantHeading(new Vector2d(-55,-55),10)
 //                .turn(Math.toRadians(225))
 //               .waitSeconds(0.7)
 //                .splineToLinearHeading(new Pose2d(58, -62, Math.toRadians(90)), Math.toRadians(0))
