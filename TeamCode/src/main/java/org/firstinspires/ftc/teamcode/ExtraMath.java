8ef6e983460c767d885df423f3e0d9e6e2b72e3a
Garnet Squadron <garnetsquadron@gmail.com>
2024-12-06 16:38:11 -0500

    added a description for a method

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ExtraMath.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ExtraMath.java
@@ -8,6 +8,14 @@ public class ExtraMath {
         double Tau = Math.PI*2;
 
     }
+
+    /**
+     * if val is below min, it returns min. if val is above max, it returns max. otherwise, it just returns val.
+     * @param val
+     * @param max
+     * @param min
+     * @return
+     */
     public static double Clamp(double val, double max, double min){
         return Math.min(Math.max(val,min),max);
     }
