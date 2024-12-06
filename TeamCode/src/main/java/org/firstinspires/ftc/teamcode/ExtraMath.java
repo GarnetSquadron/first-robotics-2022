0cc7d788a5406bea3efa74380a0c38cb678ffcd7
Garnet Squadron <garnetsquadron@gmail.com>
2024-12-06 17:02:18 -0500

    made tau public bc I forgot

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ExtraMath.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ExtraMath.java
@@ -1,13 +1,11 @@
 package org.firstinspires.ftc.teamcode;
 
 public class ExtraMath {
-    static {
-        /**
-         * Tau is superior to pi. Fight me
-         */
-        double Tau = Math.PI*2;
 
-    }
+    /**
+     * Tau is superior to pi. Fight me
+     */
+    public static double Tau = Math.PI*2;
 
     /**
      * if val is below min, it returns min. if val is above max, it returns max. otherwise, it just returns val.
