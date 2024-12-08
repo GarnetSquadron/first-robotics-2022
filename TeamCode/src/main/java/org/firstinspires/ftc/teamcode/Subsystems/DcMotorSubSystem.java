76c26f029b11b2c90b90eb8e21cc7c396aa5c22b
Garnet Squadron <garnetsquadron@gmail.com>
2024-12-08 15:13:15 -0500

    temporarily went back to the old stuff idk

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/DcMotorSubSystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/DcMotorSubSystem.java
@@ -1,12 +1,78 @@
+//package org.firstinspires.ftc.teamcode.Subsystems;
+//
+//import com.arcrobotics.ftclib.command.SubsystemBase;
+//import com.arcrobotics.ftclib.hardware.motors.Motor;
+//import com.qualcomm.robotcore.hardware.HardwareMap;
+//
+//import org.firstinspires.ftc.teamcode.ExtraMath;
+//
+///**
+// * Class to keep all DcMotor actions that can be used for multiple different motors
+// */
+//public class DcMotorSubSystem extends SubsystemBase {
+//    private final Motor motor;
+//    private final int MaxPos;
+//    private final int MinPos;
+//    private double PosCoefficient;
+//    public DcMotorSubSystem(HardwareMap hardwareMap, String MotorName,int minPos, int maxPos, double posCoefficient){
+//        motor = new Motor(hardwareMap,MotorName);
+//        motor.setZeroPowerBehavior(Motor.ZeroPowerBehavior.BRAKE);
+//        motor.resetEncoder();
+//        MaxPos = minPos;
+//        MinPos = maxPos;
+//        PosCoefficient = posCoefficient;
+//    }
+//    public void setTgPos(int posRatio){
+//        posRatio = (int)ExtraMath.Clamp(posRatio,1,0);
+//        // set the run mode
+//        motor.setRunMode(Motor.RunMode.PositionControl);
+//
+//// set and get the position coefficient
+//        motor.setPositionCoefficient(PosCoefficient);
+//
+//// set the target position
+//        motor.setTargetPosition(getPosFromRatio(MinPos, MaxPos, posRatio));      // an integer representing
+//        // desired tick count
+//
+////        motor.set(0);
+//
+//// set the tolerance
+//        motor.setPositionTolerance(13.6);   // allowed maximum error
+//
+//// perform the control loop
+//
+//    }
+//    public void runToTgPos(){
+//        if (!motor.atTargetPosition()) {
+//            motor.set(1);
+//        }
+//        else {
+//            motor.stopMotor();// stop the motor
+//        }
+//    }
+//    int getPosFromRatio(int min, int max, double pos){
+//        return (int)Math.round(min+pos*(max-min));
+//    }
+//    public boolean TargetReached(){
+//        return motor.atTargetPosition();
+//    }
+//}
+//
+//
+
+
+
+
+
+
 package org.firstinspires.ftc.teamcode.Subsystems;
 
-import com.arcrobotics.ftclib.command.SubsystemBase;
-import com.arcrobotics.ftclib.hardware.motors.Motor;
-import com.qualcomm.robotcore.hardware.HardwareMap;
-
-import org.firstinspires.ftc.teamcode.ExtraMath;
+        import com.arcrobotics.ftclib.command.SubsystemBase;
+        import com.arcrobotics.ftclib.hardware.motors.Motor;
+        import com.qualcomm.robotcore.hardware.HardwareMap;
 
 /**
+ * STILL WORKING ON THIS DO NOT USE
  * Class to keep all DcMotor actions that can be used for multiple different motors
  */
 public class DcMotorSubSystem extends SubsystemBase {
@@ -23,7 +89,6 @@ public class DcMotorSubSystem extends SubsystemBase {
         PosCoefficient = posCoefficient;
     }
     public void setTgPos(int posRatio){
-        posRatio = (int)ExtraMath.Clamp(posRatio,1,0);
         // set the run mode
         motor.setRunMode(Motor.RunMode.PositionControl);
 
@@ -31,10 +96,10 @@ public class DcMotorSubSystem extends SubsystemBase {
         motor.setPositionCoefficient(PosCoefficient);
 
 // set the target position
-        motor.setTargetPosition(getPosFromRatio(MinPos, MaxPos, posRatio));      // an integer representing
+        motor.setTargetPosition(getPosFromRatio(MinPos, MaxPos,posRatio));      // an integer representing
         // desired tick count
 
-//        motor.set(0);
+        motor.set(0);
 
 // set the tolerance
         motor.setPositionTolerance(13.6);   // allowed maximum error
@@ -51,9 +116,7 @@ public class DcMotorSubSystem extends SubsystemBase {
         }
     }
     int getPosFromRatio(int min, int max, double pos){
-        return (int)Math.round(min+pos*(max-min));
-    }
-    public boolean TargetReached(){
-        return motor.atTargetPosition();
+        return min+(int)Math.round(pos*(max-min));
     }
 }
+
