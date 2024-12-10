33d62289b3ded265d157ab43cdcf1d948928883e
Garnet Squadron <garnetsquadron@gmail.com>
2024-12-10 15:24:13 -0500

    Timer test

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Tests/TimerTest.java
@@ -0,0 +1,22 @@
+package org.firstinspires.ftc.teamcode.Tests;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.TTimer;
+
+@TeleOp(name = "timer test")
+public class TimerTest extends OpMode {
+    TTimer timer  = new TTimer(System::currentTimeMillis);
+
+    @Override
+    public void init() {
+        timer.StartTimer(100);
+    }
+
+    @Override
+    public void loop() {
+        telemetry.addData("time", System.currentTimeMillis());
+        telemetry.addData("timer up",timer.timeover());
+    }
+}
