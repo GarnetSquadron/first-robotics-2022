1e4fdb1459b39ca5f7f6bc12a8125816fc6dbb3f
Nathan14635 <nathan.keisler23@gmail.com>
2024-12-11 17:05:25 -0500

    Intake Claw push

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -66,10 +66,12 @@ import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
                     .build();
 
             //Auto routes
-            ThreeClip t = new ThreeClip(myBot); //t.run();
+            ThreeClip c = new ThreeClip(myBot); //c.run();
             ThreeSample s = new ThreeSample(myBot); //s.run();
             SoloAuto  a = new SoloAuto(myBot); //a.run();
-            VisionAuto v = new VisionAuto(myBot); //v.run()
+            VisionAuto v = new VisionAuto(myBot); //v.run();
+            //Auto routes
+
             Pose2d beginPose = new Pose2d(26,-62,Math.toRadians(90));
             Pose2d tgtPose = new Pose2d(20,20,Math.PI);
             v.run();
