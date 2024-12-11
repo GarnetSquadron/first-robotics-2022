ccd4260c982fb0ae4f86806f7cc2fdf27fa8f89f
Nathan <nathan.keisler23@gmail.com>
2024-12-10 20:34:28 -0500

    Added a solo auto prior, added a submersible vision auto

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -69,10 +69,10 @@ import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
             ThreeClip t = new ThreeClip(myBot); //t.run();
             ThreeSample s = new ThreeSample(myBot); //s.run();
             SoloAuto  a = new SoloAuto(myBot); //a.run();
-
+            VisionAuto v = new VisionAuto(myBot); //v.run()
             Pose2d beginPose = new Pose2d(26,-62,Math.toRadians(90));
             Pose2d tgtPose = new Pose2d(20,20,Math.PI);
-            a.run();
+            v.run();
 
             meepMeep.setBackground(MeepMeep.Background.FIELD_INTO_THE_DEEP_JUICE_DARK)
                     .setDarkMode(true)
---
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/VisionAuto.java
@@ -0,0 +1,43 @@
+package com.example.meepmeeptesting;
+
+import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.Vector2d;
+import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
+
+public class VisionAuto {
+    RoadRunnerBotEntity myBot;
+    Pose2d beginPose = new Pose2d(-26,-62,Math.toRadians(180));
+    VisionAuto (RoadRunnerBotEntity myBot){
+        this.myBot = myBot;
+    }
+    public void run(){
+        myBot.runAction(myBot.getDrive().actionBuilder(beginPose)
+                .splineToLinearHeading(new Pose2d(-55, -55,Math.toRadians(45)), 10)
+                .waitSeconds(0.7)
+                .splineToLinearHeading(new Pose2d(-48, -48,Math.toRadians(90)), 45)
+                .waitSeconds(0.7)
+                .splineToLinearHeading(new Pose2d(-55, -55,Math.toRadians(45)), 10)
+                .waitSeconds(0.7)
+                .splineToLinearHeading(new Pose2d(-58, -48,Math.toRadians(90)), 90)
+                .waitSeconds(0.7)
+                .splineToLinearHeading(new Pose2d(-55, -55,Math.toRadians(45)), 10)
+                .waitSeconds(0.7)
+                .splineToLinearHeading(new Pose2d(-54, -26,Math.toRadians(180)), 90)
+                .waitSeconds(0.7)
+                .setTangent(-90)
+                .splineToLinearHeading(new Pose2d(-55, -55,Math.toRadians(45)), 10)
+                .waitSeconds(0.7)
+                .splineToLinearHeading(new Pose2d(-24, -12, Math.toRadians(0)), 0)
+                .waitSeconds(1.4)
+                .setTangent(-180)
+                .splineToLinearHeading(new Pose2d(-55, -55,Math.toRadians(45)), 10)
+                .waitSeconds(0.7)
+                .splineToLinearHeading(new Pose2d(-24, -8, Math.toRadians(0)), 0)
+                .waitSeconds(1.4)
+                .setTangent(-180)
+                .splineToLinearHeading(new Pose2d(-55, -55,Math.toRadians(45)), 10)
+                .waitSeconds(0.7)
+                .splineToLinearHeading(new Pose2d(-24, -10, Math.toRadians(180)), 0)
+                .build());
+    }
+}
