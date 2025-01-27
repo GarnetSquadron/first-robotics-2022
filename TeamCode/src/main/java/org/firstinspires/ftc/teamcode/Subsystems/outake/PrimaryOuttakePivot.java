4bc3ab9ce39f3c7f1fd76ac11f3c0e80296ce544
GarnetSquadronFTC <garnetsquadron@gmail.com>
2025-01-27 17:16:23 -0500

    Second sservo changes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/outake/PrimaryOuttakePivot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/outake/PrimaryOuttakePivot.java
@@ -1,5 +1,6 @@
 package org.firstinspires.ftc.teamcode.Subsystems.outake;
 import com.acmerobotics.roadrunner.Action;
+import com.acmerobotics.roadrunner.ParallelAction;
 import com.acmerobotics.roadrunner.SequentialAction;
 import com.arcrobotics.ftclib.command.SubsystemBase;
 import com.qualcomm.robotcore.hardware.HardwareMap;
@@ -13,25 +14,36 @@ import java.util.function.DoubleSupplier;
 
 public class PrimaryOuttakePivot{
     public ActionServo pivot;
+    public ActionServo pivot2;
+
+    public Action goToPos (double ratio){
+        return new ParallelAction(pivot.runToRatio(ratio),
+        pivot2.runToRatio(ratio));
+    }
+    public Action gotodegrees (double angle){
+        return new ParallelAction(pivot.runToDegrees(angle),
+                pivot2.runToDegrees(angle));
+    }
 
     public PrimaryOuttakePivot(HardwareMap hardwareMap, DoubleSupplier time) {
          pivot = new ActionServo(hardwareMap,"primary pivot",0.9,0,2,time);
+        pivot2 = new ActionServo(hardwareMap,"primary pivot",0,0.9,2,time);
     }
 
     public Action BucketPos() {
-        return pivot.runToDegrees(135);
+        return gotodegrees(135);
     }
     public Action SpecimenOnWallPos() {
-        return pivot.runToDegrees(225);
+        return gotodegrees(225);
     }
     public Action SpecimenOnChamberPos() {
-        return pivot.runToDegrees(0);
+        return gotodegrees(0);
     }
     public Action SpecimenOnChamberPosV2() {
-        return new SequentialAction(pivot.runToDegrees(0),pivot.runToDegrees(70));
+        return new SequentialAction(gotodegrees(0),gotodegrees(70));
     }
     public Action prepareForSpecimenOnChamberPos() {
-        return pivot.runToDegrees(45);
+        return gotodegrees(45);
     }
 
     public Action TransferPos() {
