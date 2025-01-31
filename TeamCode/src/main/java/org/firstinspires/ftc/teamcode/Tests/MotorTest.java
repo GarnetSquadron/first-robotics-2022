5a2415661664a5486b30d90cb3b52d03867e3c44
Nathan14635 <nathan.keisler23@gmail.com>
2025-01-31 14:00:53 -0500

    Changed some code for motor test

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Tests/MotorTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Tests/MotorTest.java
@@ -6,7 +6,8 @@ import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.har
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.DcMotor;
+
+
 @TeleOp(name = "#JamesMotorTest")
 public class MotorTest extends OpMode {
     DcMotor motor;
@@ -15,16 +16,29 @@ public class MotorTest extends OpMode {
     public void init() {
         motor = hardwareMap.get(DcMotor.class, "testMotor");
     }
-//1425.1 ticks per rotation
+
     @Override
     public void loop() {
         motor.setPower(0);
+
         if (gamepad1.a) {
-            double startPos = motor.getCurrentPosition();
-            while(motor.getCurrentPosition() > (startPos + (1425.1/4))){
+            int startPos = motor.getCurrentPosition();
+
+            int targetPos = startPos + (int)(1425.1 / 4);
+
+            motor.setTargetPosition(targetPos);
+            motor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            motor.setPower(1);
+
+        if (motor.isBusy()) {
                 motor.setPower(1);
             }
+
+        else {
+                motor.setPower(0);
+            }
         }
+
         if (gamepad1.b) {
             motor.setPower(0);
         }
