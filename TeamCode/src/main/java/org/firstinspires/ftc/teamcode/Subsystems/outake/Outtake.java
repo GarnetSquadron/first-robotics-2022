625ace8e36b5ac2bdf8c4c6e32399839c12d2a9a
Nathan <nathan.keisler23@gmail.com>
2025-03-28 19:52:12 -0400

    changed viper height 20

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/outake/Outtake.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/outake/Outtake.java
@@ -136,7 +136,7 @@ public class Outtake {
     }
     public Action placeSpec(){
         return new SequentialAction(
-                vipers.goToInches(3.48),
+                vipers.goToInches(3.46),
                 new SleepAction(0.15),
                 new ParallelAction(
                 pivot1.goToRad(Math.toRadians(150)),
