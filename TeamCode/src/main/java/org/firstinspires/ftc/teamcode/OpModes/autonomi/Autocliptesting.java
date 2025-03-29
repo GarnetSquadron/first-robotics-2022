53481f7a1f0598afc33c3953347e3b2dbfe81f22
Nathan <nathan.keisler23@gmail.com>
2025-03-28 20:43:32 -0400

    changed auto pos 26

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/autonomi/Autocliptesting.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/autonomi/Autocliptesting.java
@@ -97,9 +97,9 @@ public class Autocliptesting extends LinearOpMode {
                 .splineToLinearHeading(new Pose2d(5, SubDriveInPos, Math.toRadians(270)), Math.toRadians(90));
 
         TrajectoryActionBuilder WallGrab2 = Depositdrivein1.endTrajectory().fresh()
-                .setTangent(Math.toRadians(270))
+                .setTangent(Math.toRadians(30))
                 .splineToLinearHeading(new Pose2d(8, -45, Math.toRadians(270)), Math.toRadians(180))
-                .setTangent(Math.toRadians(-10))
+                .setTangent(Math.toRadians(270))
                 .splineToLinearHeading(new Pose2d(prepWallPos.minus(new Vector2d(0.5,0)), Math.toRadians(90)), Math.toRadians(-90))
                 .waitSeconds(0.1)
                 .splineToLinearHeading(new Pose2d(WallPos.minus(new Vector2d(0.5,0)), Math.toRadians(90)), Math.toRadians(-90),new TranslationalVelConstraint(10));
