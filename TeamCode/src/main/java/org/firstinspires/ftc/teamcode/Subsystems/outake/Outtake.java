98c88b415682ce3d0517f3f515090aff435d24f6
Nathan <nathan.keisler23@gmail.com>
2025-04-04 20:08:52 -0400

    tuning auto 7

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/outake/Outtake.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/outake/Outtake.java
@@ -139,7 +139,7 @@ public class Outtake {
                 vipers.goToInches(3.40),
                 new SleepAction(0.05),
                 new ParallelAction(
-                pivot1.goToRad(Math.toRadians(154)),
+                pivot1.goToRad(Math.toRadians(157)),
                 pivot2.goToDegrees(60)
                 ));
     }
